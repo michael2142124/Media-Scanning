fff283bea5022f23c11fbc2e616c9fe666d63f6b
michael2142124 <michaelrtclare@gmail.com>
2025-06-27 15:32:25 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# Install dependencies
 apt-get update
-apt-get install -y wget gnupg2 apt-transport-https curl unzip
+apt-get install -y wget curl gnupg2 apt-transport-https unzip
 
-# Install Chrome
+# Download and install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
 
-# Confirm binary path
-which google-chrome
+# Move Chrome binary to standard location
+ln -s /usr/bin/google-chrome-stable /usr/bin/google-chrome
